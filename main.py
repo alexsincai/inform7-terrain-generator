58c0e800d8e56528fb67035c14050cb079c6ed08
alexsincai <alex.sincai@yahoo.co.uk>
2022-01-06 14:49:12 +0200

    not quite working

--- main.py
+++ main.py
@@ -47,9 +47,14 @@ class TerrainMaker:
         octaves: float = 3,
         seed: int = 3,
         name: str = "Room",
-        description: str = "[room-description]",
+        description: str = "room description",
+        initial: str = "Some initial description here",
         region: str = "Region",
-        properties: List[str] = ["black", "grey", "white"],
+        properties: Dict[str, str] = {
+            "black": "The room is black",
+            "grey": "The rrom is grey",
+            "white": "The room is white",
+        },
     ):
         self.width = max(1, width)
         self.height = max(1, height)
@@ -57,6 +62,7 @@ class TerrainMaker:
         self.seed = seed
         self.name = name
         self.description = description
+        self.initial = initial
         self.region = region
         self.properties = properties
 
@@ -96,17 +102,31 @@ class TerrainMaker:
                     len(self.properties) - 1,
                 )
                 east = f"{self.name} {words(index - 1)}".title() if width > 1 else None
+                west = (
+                    f"{self.name} {words(index + 1)}".title()
+                    if width <= self.width
+                    else None
+                )
+                north = (
+                    f"{self.name} {words(index - height + 1)}".title()
+                    if height > 1
+                    else None
+                )
                 south = (
                     f"{self.name} {words(index - height - 1)}".title()
                     if height > 1
                     else None
                 )
 
+                keys = list(self.properties.keys())
+
                 out.append(
                     {
                         "name": f"{self.name} {words(index)}".title(),
-                        "value": self.properties[round(value)],
+                        "value": keys[round(value)],
                         "east": east,
+                        "west": west,
+                        "north": north,
                         "south": south,
                     }
                 )
@@ -124,37 +144,51 @@ class TerrainMaker:
             str: The generated list of rooms, with properties, connections and optional region.
         """
         out = []
-        length = len(self.properties)
+        length = len(self.properties.keys())
+        keys = list(self.properties.keys())
 
         if length > 2:
             props = ", ".join(
-                self.properties[0:-1] + [f"or {self.properties[-1]}"]
-                if length > 1
-                else self.properties
+                keys[0:-1] + [f"or {keys[-1]}"] if length > 1 else keys
             ).replace(", or", " or")
 
         elif length == 2:
-            props = " or ".join(self.properties)
+            props = " or ".join(keys)
 
         else:
-            props = "".join(self.properties)
+            props = "".join(keys)
 
-        out.append(f"A room can be {props}. A room is usually {self.properties[0]}.")
+        out.append(f"A room can be {props}. A room is usually {keys[0]}.")
+
+        properties = []
+        properties.append(f"To say {self.description}:")
+        properties.append(f'\tsay "{self.initial}[run paragraph on]";')
+
+        for prop, desc in self.properties.items():
+            properties.append(f'\tif the location is {prop}, say "{desc}";')
+
+        properties = "\n".join(properties) + "."
+        properties = properties.replace(";.", ".")
+
+        out.append(properties)
 
         for room in self.__rooms():
-            room = f"{room.get('name')} is a room. "
-            room += f'The description is "{self.description}". '
-            room += f"It is {room.get('value')}. "
+            this_room = f"{room.get('name')} is a room. "
+            this_room += f'The description is "[{self.description}]". '
+            this_room += f"It is {room.get('value')}. "
 
             east = f"east of {room.get('east')}" if room.get("east") else None
+            west = f"west of {room.get('west')}" if room.get("west") else None
+            north = f"north of {room.get('north')}" if room.get("north") else None
             south = f"south of {room.get('south')}" if room.get("south") else None
-            directions = [east, south]
+
+            directions = [east, west, north, south]
             directions = [d for d in directions if d]
 
             if len(directions):
-                room += f"{room.get('name')} is { ' and '.join(directions) }."
+                this_room += f"{room.get('name')} is { ' and '.join(directions) }."
 
-            out.append(f"{room.strip()}")
+            out.append(f"{this_room.strip()}")
 
         if region:
             rooms = [r.get("name") for r in self.__rooms()]
@@ -170,7 +204,7 @@ class TerrainMaker:
                 + f"{rooms} {'are' if length > 1 else 'is'} in {self.region}."
             )
 
-        return "\n\n".join([o.strip() for o in out if len(o.strip())])
+        return "\n\n".join(out)
 
 
 def main():
@@ -178,11 +212,18 @@ def main():
     Create a wilderness
     """
     terrain = TerrainMaker(
-        width=5,
-        height=4,
+        width=2,
+        height=2,
         name="Wilderness",
         region="Wilds",
-        properties=["flat", "hilly", "impacted", "treed"],
+        description="wilderness-description",
+        initial="[one of]Scrubland[or]Martian scrubland[or]Martian weeds[at random] [stretch] as far as the eye [can] see.",
+        properties={
+            "flat": "The land here is flat.",
+            "hilly": "The land is somewhat hilly",
+            "impacted": "A [one of]large[or]eroded[or]small[at random] crater mars the surface.",
+            "treed": "A lone tree dominates the landscape.",
+        },
     )
     print(terrain.print())
 
