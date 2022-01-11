55b9e0dc06e2160b22905e9f154105e333ac3fac
alexsincai <alex.sincai@yahoo.co.uk>
2022-01-11 13:44:10 +0200

    Simplified generator

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-"""
-Creates a list of Inform7 rooms, all connected, which have related features.
-"""
-
-from typing import Dict, List, Tuple, Optional, Union
+"""Creates connected Inform7 rooms"""
+from typing import List, Union
 from perlin_noise import PerlinNoise
 from num2words import num2words as words
 
@@ -14,19 +11,18 @@ def translate_value(
     to_min: Union[int, float] = 0,
     to_max: Union[int, float] = 1,
 ) -> float:
-    """Maps a value from one range to another
+    """Maps value from one range to another
 
     Args:
         value (Union[int, float]): The value to be mapped
-        from_min (Union[int, float], optional): The minimum of the source range. Defaults to 0.
-        from_max (Union[int, float], optional): The maximum of the source range. Defaults to 1.
-        to_min (Union[int, float], optional): The minimum of the target range. Defaults to 0.
-        to_max (Union[int, float], optional): The maximum of the target range. Defaults to 1.
+        from_min (Union[int, float], optional): Input minimum. Defaults to 0.
+        from_max (Union[int, float], optional): Input maximum. Defaults to 1.
+        to_min (Union[int, float], optional): Output minimum. Defaults to 0.
+        to_max (Union[int, float], optional): Output maximum. Defaults to 1.
 
     Returns:
-        float: The value mapped to the [to_min, to_max] range
+        float: Mapped value
     """
-
     from_span = from_max - from_min
     to_span = to_max - to_min
 
@@ -35,350 +31,165 @@ def translate_value(
     return to_min + (ret * to_span)
 
 
-def pretty_list(
-    array: List[str] = ["foo", "bar", "baz"],
-    andor: str = "or",
-) -> str:
-    """Prettyfies a list
-
-    Args:
-        array (List[str], optional): A list of strings to join. Defaults to ["foo", "bar", "baz"].
-        andor (str, optional): The last element conjunction. Defaults to "or".
-
-    Returns:
-        str: The resulting string
-    """
-    length = len(array)
-
-    if not length:
-        return ""
-
-    if length == 1:
-        return array[0]
-
-    if length == 2:
-        return f" {andor} ".join(array)
-
-    temp = array[:-1] + [f"{andor} {array[-1]}"]
-    out = ", ".join(temp)
-
-    return out.replace(f", {andor}", f" {andor}")
-
-
 class Room:
-    """
-    A room prepresentation
-    """
+    """Creates Inform7 room"""
 
-    def __init__(
-        self,
-        name: str = "Room",
-        value: float = 0.0,
-        condition: str = "room condition",
-        position: Tuple[int, int] = (0, 0),
-        description: str = "The room description",
-        printed_name: str = "Room",
-        north: Optional[str] = None,
-        south: Optional[str] = None,
-        east: Optional[str] = None,
-        west: Optional[str] = None,
-    ):
-        self.name = name
-        self.value = value
-        self.condition = condition
-        self.position = position
-        self.description = description
-        self.printed_name = printed_name
-        self.north = north
-        self.south = south
-        self.east = east
-        self.west = west
-
-    def render(self) -> str:
-        """Generates an Inform7 room.
-
-        Returns:
-            str: The code for an Inform7 room.
-        """
-        north = f"north of {self.north}" if self.north else None
-        south = f"south of {self.south}" if self.south else None
-        east = f"east of {self.east}" if self.east else None
-        west = f"west of {self.west}" if self.west else None
-
-        neighbors = [north, south, east, west]
-        neighbors = [d for d in neighbors if d]
-
-        name = f"{self.name} is a room."
-        condition = f"It is {self.condition}."
-        description = f'The description of {self.name} is "[{self.description}]".'
-        printed = f'The printed name is "{self.printed_name}".'
-        directions = (
-            f"{self.name} is {pretty_list(array=neighbors, andor='and')}."
-            if len(neighbors)
-            else ""
-        )
-
-        return " ".join([name, condition, description, printed, directions])
-
-
-class Region:
-    """
-    A Region representation
-    """
-
-    def __init__(
-        self,
-        name: str = "Region",
-        condition: str = "particular",
-        rooms: List[str] = [],
-    ):
-        self.name = name
-        self.condition = condition
-        self.rooms = rooms
-
-    def render(self) -> str:
-        """Generates an Inform7 region.
-
-        Returns:
-            str: The generated region
-        """
-        name = f"{self.name} is a region."
-        rooms = pretty_list(array=self.rooms, andor="and")
-        isare = "is" if len(self.rooms) == 1 else "are"
-
-        return " ".join([name, rooms, isare, f"in {self.name}."])
-
-
-class TerrainMaker:
-    """
-    Creates a list of Inform7 rooms, all connected, which have related features.
-    """
-
-    def __init__(
-        self,
-        width: int = 6,
-        height: int = 5,
-        seed: int = 3,
-        name: str = "Room",
-        description: str = "room description",
-        initial: str = "Some initial description here",
-        region: str = "Region",
-        printed_name: str = "Room",
-        conditions: Dict[str, str] = {
-            "black": "The room is black",
-            "grey": "The room is grey",
-            "white": "The room is white",
-        },
-    ):
-        self.width = max(1, width)
-        self.height = max(1, height)
-        self.seed = seed
-        self.name = name
-        self.description = description
-        self.initial = initial
+    def __init__(self, name, index, region):
+        self._name = name
+        self.index = words(index).title()
+        self.name = f"{name} {words(index).title()}"
         self.region = region
-        self.printed_name = printed_name
-        self.conditions = conditions
+        self.east = None
+        self.north = None
 
     def __str__(self):
-        return self.render()
+        out = []
+        out.append(f"{self.name} is a room.")
+        out.append(f"The printed name is '{self._name}'.")
 
-    def noise(self, horizontal: float = 0.0, vertical: float = 0.0) -> float:
-        """Generates a 2D noise map
+        dirs = []
+        dirs.append(f"east of {self.east}" if self.east else None)
+        dirs.append(f"north of {self.north}" if self.north else None)
+        dirs = [d for d in dirs if d]
+
+        if len(dirs):
+            out.append(f"It is { ' and '.join(dirs)}.")
+
+        return " ".join(out)
+
+
+class Terrain:
+    """Creates a series of interconnected Inform7 rooms, assembled into several regions"""
+
+    def __init__(
+        self,
+        width,
+        height,
+        seed,
+        name,
+        regions,
+        parent,
+        entry_index=None,
+        entry_name=None,
+    ):
+        self.width = width
+        self.height = height
+        self.seed = seed
+        self.name = name
+        self.regions = regions
+        self.parent = parent
+        self.entry_index = entry_index
+        self.entry_name = entry_name
+
+        self.rooms = self.build_rooms()
+
+    def noise(
+        self,
+        horizontal: Union[int, float] = 0.0,
+        vertical: Union[int, float] = 0.0,
+    ) -> int:
+        """Generates Perlin Noise for given coordinates
 
         Args:
-            horizontal (float, optional): X component. Defaults to 0.0.
-            vertical (float, optional): Y component. Defaults to 0.0.
+            horizontal (Union[int, float], optional): Horizontal offset. Defaults to 0.0.
+            vertical (Union[int, float], optional): Vertical offset. Defaults to 0.0.
 
         Returns:
-            float: Noise value for current cell
+            int: Noise value at given coordinates
         """
-        noise1 = PerlinNoise(octaves=3, seed=self.seed)
-        noise2 = PerlinNoise(octaves=6, seed=self.seed)
-        noise3 = PerlinNoise(octaves=12, seed=self.seed)
-        noise4 = PerlinNoise(octaves=24, seed=self.seed)
+        noise1 = PerlinNoise(octaves=2, seed=self.seed)
+        noise2 = PerlinNoise(octaves=4, seed=self.seed)
+        noise3 = PerlinNoise(octaves=8, seed=self.seed)
+        noise4 = PerlinNoise(octaves=16, seed=self.seed)
+        noise5 = PerlinNoise(octaves=32, seed=self.seed)
 
-        out = noise1([horizontal, vertical])
-        out += noise2([horizontal, vertical]) * 0.5
-        out += noise3([horizontal, vertical]) * 0.25
-        out += noise4([horizontal, vertical]) * 0.125
+        ret = noise1([horizontal + 1 / self.width, vertical + 1 / self.height])
+        ret += noise2([horizontal + 1 / self.width, vertical + 1 / self.height])
+        ret += noise3([horizontal + 1 / self.width, vertical + 1 / self.height]) * 0.5
+        ret += noise4([horizontal + 1 / self.width, vertical + 1 / self.height]) * 0.25
+        ret += noise5([horizontal + 1 / self.width, vertical + 1 / self.height]) * 0.125
+        ret += 0.5
 
-        return out + 0.5
+        return round(translate_value(ret, 0, 1, 0, len(self.regions) - 1))
 
-    def __values(self) -> List[float]:
+    def build_rooms(self)->List[Room]:
+        """Generates list of Inform7 rooms
+
+        Returns:
+            List[Room]: List of Rooms for the terrain
+        """
+        index = 1
         out = []
 
-        for height in range(1, self.height + 1):
-            for width in range(1, self.width + 1):
-                noise = self.noise(width / self.width + 1, height / self.height + 1)
-                out.append(noise)
+        for height in range(self.height):
+            for width in range(self.width):
+                noise = self.noise(width, height)
+                room = Room(self.name, index, self.regions[noise])
 
-        return out
+                if width >= 1:
+                    room.east = f"{self.name} {words(index - 1).title()}"
 
-    def __rooms(self) -> List[Dict[str, Optional[str]]]:
-        out = []
-        index = 0
+                if height >= 1:
+                    room.north = f"{self.name} {words(index - 5).title()}"
 
-        values = self.__values()
-        conditions = self.conditions
+                if self.entry_index and index == self.entry_index:
+                    room.north = self.entry_name
 
-        for height in range(1, self.height + 1):
-            for width in range(1, self.width + 1):
+                out.append(room)
                 index += 1
 
-                value = translate_value(values[index - 1], 0, 1, 0, len(conditions) - 1)
-                position = (width - 1, height - 1)
-                keys = list(conditions.keys())
-                count = (self.width * self.height) + 1
-
-                out.append(
-                    Room(
-                        name=f"{self.name} {words(count - index)}".title(),
-                        value=value,
-                        condition=keys[round(value)],
-                        position=position,
-                        description=self.description,
-                        printed_name=self.printed_name,
-                    )
-                )
-
-        for room in out:
-            pos_x, pos_y = room.position
-
-            if pos_y < self.height:
-                to_south = [r for r in out if r.position == (pos_x, pos_y + 1)]
-                room.north = to_south.pop().name if len(to_south) else None
-
-            if pos_y > 0:
-                to_north = [r for r in out if r.position == (pos_x, pos_y - 1)]
-                room.south = to_north.pop().name if len(to_north) else None
-
-            if pos_x < self.width:
-                to_east = [r for r in out if r.position == (pos_x + 1, pos_y)]
-                room.west = to_east.pop().name if len(to_east) else None
-
-            if pos_x > 0:
-                to_west = [r for r in out if r.position == (pos_x - 1, pos_y)]
-                room.east = to_west.pop().name if len(to_west) else None
-
         return out
 
-    def __regions(self):
-        out = []
+    def __str__(self):
+        out = ["\n".join(str(r) for r in self.rooms)]
 
-        for key in list(self.conditions.keys()):
-            name = f"The {key} {self.region}".title()
-            rooms = [r.name for r in self.__rooms() if r.condition == key]
+        for region in self.regions:
+            desc = [f"{region} {self.name} is a region."]
+            rooms = [room.name for room in self.rooms if room.region == region]
 
             if len(rooms):
-                out.append(
-                    Region(
-                        name=name,
-                        condition=key,
-                        rooms=rooms,
-                    )
-                )
+                desc.append(f"{ ', '.join(rooms)} are in {region} {self.name}.")
+                out.append(" ".join(desc))
 
-        return out
+        parent = [f"{self.parent} {self.name} is a region."]
+        regions = ", ".join([f"{region} {self.name}" for region in self.regions])
+        parent.append(f"{regions} are in {self.parent} {self.name}.")
 
-    def __conditions(self):
-        regions = [r.condition for r in self.__regions()]
-        conditions = [c for c in list(self.conditions.keys()) if c in regions]
+        out.append(f"{' '.join(parent)}")
 
-        out = []
-
-        if len(conditions):
-            intro = []
-            intro.append(f"A room can be {pretty_list(array=conditions, andor='or')}.")
-            intro.append(f"A room is usually {conditions[0]}.")
-
-            out.append(" ".join(intro))
-            out.append("")
-
-        out.append(f"To say {self.description}:")
-        out.append(f'\tsay "{self.initial} [run paragraph on]";')
-
-        for index, condition in enumerate(conditions):
-            description = self.conditions.get(condition)
-            last = "." if index == len(conditions) - 1 else ";"
-
-            out.append(f'\tif the location is {condition}, say "{description}"{last}')
-
-        return "\n".join(out).strip()
+        return "\n".join(out)
 
     def test(self) -> str:
-        """Generates a test map with a maximum of 5 characters
+        """Outputs test map
 
         Returns:
-            str: A gradient
+            str: The test map
         """
-        chars = " .-+#"
         out = ""
 
-        index = 1
-        values = self.__values()
-
-        for _ in range(self.height):
-            for _ in range(self.width):
-                value = translate_value(values[index - 1], 0, 1, 0, len(chars) - 1)
-                out += chars[round(value)]
-                index += 1
-
-            out += "\n"
+        for index, room in enumerate(self.rooms):
+            out += room.region[0]
+            if not (index + 1) % self.width:
+                out += "\n"
 
         return out
-
-    def render(self) -> str:
-        """Outputs the list of generated rooms, as well as the regions
-
-        Returns:
-            str: The generated list of rooms, with conditions and connections as well as the regions
-        """
-        out = []
-
-        out.append(self.__conditions())
-
-        for room in self.__rooms():
-            out.append(room.render())
-
-        for region in self.__regions():
-            out.append(region.render())
-
-        temp = f"{self.region} is a region. "
-        regions = [r.name for r in self.__regions()]
-        isare = "is" if len(regions) == 1 else "are"
-
-        temp += pretty_list(array=regions, andor="and")
-        temp += f" {isare} in "
-        temp += f"{self.region}."
-
-        out.append(temp)
-
-        return "\n\n".join(out)
 
 
 def main():
-    """
-    Create a wilderness
-    """
-    terrain = TerrainMaker(
-        width=3,
+    """Main generator function"""
+    terrain = Terrain(
+        width=5,
         height=4,
-        seed=244,
-        name="Gharraz",
-        region="Gharraz",
-        printed_name="A street of Gharraz",
-        description="gharraz-street-description",
-        initial="",
-        conditions={
-            "ruined": "A poor street.",
-            "poor": "A poor street.",
-            "clean": "A clean street",
-            "prosperous": "A prosperos street",
-            "rich": "A large plaza",
-        },
+        seed=7362,
+        name="Wilderness",
+        regions=["Rocky", "Impacted", "Flat", "Vegetated"],
+        parent="Northern",
+        entry_index=3,
+        entry_name="the weed-choked street",
     )
-    print(terrain.test())
+    terrain = str(terrain).replace("'", '"')
+
+    print(terrain)
 
 
 if __name__ == "__main__":
